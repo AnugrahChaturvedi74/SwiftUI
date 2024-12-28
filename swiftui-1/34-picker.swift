0921dbfc6ef60da58300f8f76a14a14c5d68fab8
Anugrah <anugrah.chaturvedi@binarysemantics.com>
2024-12-28 18:50:12 +0530

    sd

---
+++ swiftui-1/34-picker.swift
@@ -0,0 +1,18 @@
+//
+//  34-picker.swift
+//  swiftui-1
+//
+//  Created by Anugrah on 06/08/24.
+//
+
+import SwiftUI
+
+struct _4_picker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _4_picker()
+}
---
+++ swiftui-1/35-tabView.swift
@@ -0,0 +1,18 @@
+//
+//  35-tabView.swift
+//  swiftui-1
+//
+//  Created by Anugrah on 06/08/24.
+//
+
+import SwiftUI
+
+struct _5_tabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _5_tabView()
+}
